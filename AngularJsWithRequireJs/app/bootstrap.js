37eaf76c397a3bba31410cc2245cd102a44ce0e3
ridvan.cetin <ridvan.cetin@projectbeagle.com>
2017-04-19 17:04:40 +0300

    Add project files.

---
+++ AngularJsWithRequireJs/app/bootstrap.js
@@ -0,0 +1,30 @@
+﻿require.config({
+    baseUrl: "/app",
+    paths: {
+        "angular": "/scripts/angular",
+        "angular-route": "/scripts/angular-route",
+        "angular-cookies": "/scripts/angular-cookies",
+        "jquery": "/scripts/jquery.min",
+    },
+    shim: {
+        "app": {
+            deps: ["angular", "angular-route", "angular-cookies", "jquery"]
+        },
+        "angular-route": {
+            deps: ["angular"]
+        },
+        "angular-cookies": {
+            deps: ["angular"]
+        }
+
+    }
+});
+require
+(
+    [
+        "app"
+    ],
+    function (app) {
+        angular.bootstrap(document, ["app"]);
+    }
+);
---
+++ AngularJsWithRequireJs/app/routes.js
@@ -0,0 +1,26 @@
+﻿define([], function () {
+    return {
+        defaultRoutePath: '/',
+        routes: {
+            '/': {
+                templateUrl: '/partials/home.html',
+                dependencies: [
+                    'controllers/HomeViewController'
+                ]
+            },
+            '/products': {
+                templateUrl: '/partials/products.html',
+                dependencies: [
+                    'controllers/ProductViewController'
+                ]
+            },
+            '/about/:person': {
+                templateUrl: '/partials/about.html',
+                dependencies: [
+                    'controllers/AboutViewController',
+                    'directives/app-color'
+                ]
+            }
+        }
+    };
+});
